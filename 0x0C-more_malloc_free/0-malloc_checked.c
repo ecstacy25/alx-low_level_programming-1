ee849622bdcd3176c7ba2045ae7fa442302e106e
UdeeA1 <udyamaku@gmail.com>
2023-04-13 12:15:07 -0700

    fixed finally

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,13 +3,13 @@
 /**
  * *malloc_checked - allocates memory using malloc and exit if failed
  * @b: int
- * Return: pointer to the array initialized or NULL
+ * Return: pointer to the allocated memory or if NULL 98
  */
 
 void *malloc_checked(unsigned int b)
 {
-int *m = malloc(b);
-if (m == 0)
+void *m = malloc(b);
+if (m == NULL)
 	exit(98);
 
 return (m);
